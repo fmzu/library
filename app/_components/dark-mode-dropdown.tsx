fb6976cab3cbfc60096dd9f29fab0f1ad6f7890f
fmzu <149039422+fmzu@users.noreply.github.com>
2024-03-11 10:37:34 +0900

    update: ページを修正する

--- app/_components/dark-mode-dropdown.tsx
+++ app/_components/dark-mode-dropdown.tsx
@@ -22,7 +22,7 @@ export const DarkModeDropdown = () => {
           <span className="sr-only">{"Toggle theme"}</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="center">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           {"Light"}
         </DropdownMenuItem>
